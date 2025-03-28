ae0ceb27350928951b8a05b236575a471420242e
Rohit Pancholi <rohitpancholi14510@gmail.com>
2025-03-28 12:07:40 +0530

    chng

--- script.js
+++ script.js
@@ -72,7 +72,13 @@ var swiper = new Swiper(".mySwiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  autoplay: {
+    delay: 2000, 
+    disableOnInteraction: false, 
+  },
+  loop: true, 
 });
+
 
 //  shop-by-category-slider
 var swiper = new Swiper(".shop_by_categories", {
@@ -125,6 +131,33 @@ var swiper = new Swiper(".shop_by_categories", {
       },
       1024: {
         slidesPerView: 7,   /* Desktop */
+        spaceBetween: 20
+      }
+    }
+  });
+  //Top-Rated-Product
+  var swiper = new Swiper(".Top-Rated-Product", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,   /* Mobile */
+        spaceBetween: 10
+      },
+      768: {
+        slidesPerView: 3,   /* Tablet */
+        spaceBetween: 15
+      },
+      1024: {
+        slidesPerView: 4,   /* Desktop */
         spaceBetween: 20
       }
     }
